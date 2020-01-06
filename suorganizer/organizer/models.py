a32d18295640a5e812849f31fc27530962e80df7
Sterling De Jesus <admin@admin.com>
2020-01-06 11:06:07 -0400

    Chapter 3 - 3.4: Specifying and organizing data in models

--- suorganizer/organizer/models.py
+++ suorganizer/organizer/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from blog.models import Blog
 
-# Create your models here.
+
+
+
+
+class Tag(models.Model):
+    name    = models.CharField(max_length=31
+                               unique=True)
+    slug    = models.SlugField(max_length=31,
+                               unique=True,
+                               help_text='A label to URL config.')
+
+    class Meta:
+        ordering    = ['name']
+    
+    def __str__(self):
+        return self.name
+
+
+class Startup(models.Model):
+    name    = models.CharField(max_length=31,
+                               db_index=True)
+    slug    = models.SlugField(max_length=31,
+                               unique=True,
+                               help_text='A label for URL config.')
+    description     = models.TextField()
+    founded_date    = models.DateField('date founded')
+    contact     = models.EmailField()
+    website     = models.URLField(max_length=255)
+    tags    = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering    = ['name']
+        get_latest_by   = 'founded_date'
+    
+    def __str__(self):
+        return self.name
+
+
+class NewsLink(models.Model):
+    title   = models.CharField(max_length=63)
+    pub_date    = models.DateField('date published')
+    link    = models.URLField(max_length=255)
+    startup     = models.ForeignKey(Startup)
+
+    class Meta:
+        verbose_name    = 'news article'
+        ordering    = ['-pub_date']
+        get_latest_by   = 'pub_date'
+    
+    def __str__(self):
+        return "{}:{}".format(self.startup, self.title)
